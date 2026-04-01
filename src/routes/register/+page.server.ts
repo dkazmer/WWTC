12d7c6f234c83f43f352e588dcaaf467ff6565d8
Daniel Kazmer <dkazmer@live.ca>
2026-04-01 02:07:59 -0400

    construct reg email

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -1,8 +1,9 @@
+import { fail } from '@sveltejs/kit';
 import { drizzle } from 'drizzle-orm/mysql2';
 import { DATABASE_URL } from '$env/static/private';
 import { getTable, type TableDB } from '$lib/server/db/schema';
 import { tryCatch } from '$lib/try-catch.js';
-import { fail } from '@sveltejs/kit';
+import { send } from './email.js';
 
 const db = drizzle(DATABASE_URL);
 const defaultTable = getTable('reg2026');
@@ -23,6 +24,7 @@ export const actions = {
 				case 'street': formattedData.address = value as string; break;
 				case 'city': formattedData.address += `, ${value}` as string; break;
 				case 'postalCode': formattedData.postal = value as string; break;
+				case 'returning': formattedData.bType = value as TableDB['bType']; break;
 				case 'owing': formattedData.owing = parseInt(value as string, 10); break;
 				case 'send-money': break;
 				// @ts-expect-error (ts7053): can't use `string` to index...
@@ -33,16 +35,20 @@ export const actions = {
 		formattedData.numApplicants = additionalFamily.length + 1;
 		console.log('>> formattedData', formattedData, additionalFamily);
 
-		const { data, error } = await tryCatch(db.insert(defaultTable).values(formattedData));
+		// const { data, error } = await tryCatch(db.insert(defaultTable).values(formattedData));
 
-		if (error) return fail(400, { message: error.message, cause: error.cause });
+		// if (error) {
+		// 	const { message, cause } = error;
+		// 	return fail(400, { message, cause });
+		// }
 
 		// insert additional family
 		// additionalFamily.forEach(member => {
 		// 	db.insert(defaultTable).values(member);
 		// });
 
-		// send email
+		// email
+		send(formattedData);
 	}
 };
 
@@ -54,8 +60,8 @@ function insertFamily(formData: FormData) {
 		formData.get(`firstName[${i}]`) && arr.push({
 			firstName: formData.get(`firstName[${i}]`),
 			lastName: formData.get(`lastName[${i}]`),
-			gender: formData.get(`gender[${i}]`),
-			ageGroup: formData.get(`ageGroup[${i}]`)
+			ageGroup: formData.get(`ageGroup[${i}]`),
+			gender: formData.get(`gender[${i}]`)
 		} as TableDB);
 	}
 
---
+++ src/routes/register/email.ts
@@ -0,0 +1,52 @@
+import { createTransport, type SendMailOptions } from 'nodemailer';
+import { DB_PASS } from '$env/static/private';
+import { season } from '$lib/constants';
+import type { TableDB } from '$lib/server/db/schema';
+
+const senderAlt = 'wwtcspri@sv32.byethost32.org';
+const sender = 'wishingwelltennis@hotmail.com';
+
+const transporter = createTransport({
+	host: 'sv32.byethost32.org',
+	port: 587,
+	from: `Wishing Well Tennis Club <${senderAlt}>`,
+	replyTo: `Wishing Well Tennis Club <${sender}>`,
+	headers: {
+		'MIME-Version': '1.0',
+		'Content-type': 'text/plain; charset=UTF-8'
+	},
+	auth: {
+		// user: senderAlt,
+		user: 'wwtcspri',
+		pass: decodeURIComponent(DB_PASS)
+	}
+});
+
+export function send(userData: TableDB) {
+	const mailOptions: SendMailOptions = {
+		to: userData.email,
+		from: `Wishing Well Tennis Club <${senderAlt}>`,
+		replyTo: `Wishing Well Tennis Club <${sender}>`,
+		// bcc: `dkazmer@live.ca; ${sender}`,
+		subject: `${season} tennis season at WWTC`,
+		// text: getBody(),
+		html: getBody(userData)
+	};
+
+	transporter.sendMail(mailOptions, (error, info) => {
+		error ? console.error('>> error', error) : console.log('>> email sent!', info);
+	});
+}
+
+function getBody({ firstName, lastName, owing, bType }: TableDB) {
+	return `Welcome${bType === 'returning' ? ' back' : ''}, <b>${firstName} ${lastName}</b>, to <i>Wishing Well Tennis Club</i> for the ${season} season!
+<p>
+Please remember to send a money transfer in the amount of <b>$${owing}</b> to <u>${sender}</u>.
+</p>
+Once we have received payment, we'll provide the gate combo-lock code, and you'll be able to collect your shoe tag on opening day or thereafter. Allow twenty-four (24) hours to process your membership.
+<p>
+If this message landed in your junk/spam folder, mark us as a safe sender in order to ensure receipt of future messages. We thank you for signing up and hope to see you soon!
+</p>
+<img src="https://material-icons.github.io/material-icons/svg/sports_tennis/outline.svg" width="24" height="24" style="width:24px;max-width:24px;filter:invert(28%) sepia(93%) saturate(1245%) hue-rotate(70deg) brightness(133%) contrast(93%);" />
+<p>WWTC team</p>`;
+}
